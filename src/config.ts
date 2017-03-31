067699e825a5c1c40dda48ca1ffa26b7922061b3
Sofia Atsalou <sofia.atsalou@gmail.com>
2017-03-31 18:16:28 +0300

    feat(paypal) Send the PayPal payment

--- src/config.ts
+++ src/config.ts
@@ -3,4 +3,7 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class Config {
 	public wordpressApiUrl = 'http://demo.titaniumtemplates.com/wordpress/?json=1';
+	// Paypal
+	static payPalEnvironmentSandbox = 'AXg409-ZD7lFcgk2JdHkLkggX8u7LnT7cfkGL2AG0y7bx5OAvOmErpKKz5D68kzXRxbfe_KRlFf681rk';
+	static payPalEnvironmentProduction = '';
 }
--- src/pages/paypal/paypal.page.ts
+++ src/pages/paypal/paypal.page.ts
@@ -1,9 +1,32 @@
 import { Component } from '@angular/core';
+import { PayPal, PayPalPayment, PayPalConfiguration } from 'ionic-native';
+import { Config } from '../../config';
 
 @Component({
 	templateUrl: 'paypal.html'
 })
 
 export class PayPalPage {
+	payment: PayPalPayment = new PayPalPayment('10.10', 'USD', 'TV', 'sale');
+	currencies = ['EUR', 'USD']
 
+	makePayment() {
+		PayPal.init({
+			PayPalEnvironmentProduction: Config.payPalEnvironmentProduction,
+			PayPalEnvironmentSandbox: Config.payPalEnvironmentSandbox
+		}).then(() => {
+			PayPal.prepareToRender('PayPalEnvironmentSandbox', new PayPalConfiguration({})).then(() => {
+				PayPal.renderSinglePaymentUI(this.payment).then((response) => {
+					alert(`Successfully paid. Status = ${response.response.state}`);
+					console.log(response);
+				}, () => {
+					console.error('Error or render dialog closed without being successful');
+				});
+			}, () => {
+				console.error('Error in configuration');
+			});
+		}, () => {
+			console.error('Error in initialization, maybe PayPal isn\'t supported or something else');
+		});
+	}
 }
